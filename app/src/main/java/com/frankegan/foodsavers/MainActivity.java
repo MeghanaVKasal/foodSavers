0bcf87e657f9d5a8e79b00d1879738c6be1ab6e5
fegan104 <fegan104@gmail.com>
2018-12-05 13:49:48 -0500

    notifications are working

--- app/src/main/java/com/frankegan/foodsavers/MainActivity.java
+++ app/src/main/java/com/frankegan/foodsavers/MainActivity.java
@@ -3,7 +3,6 @@ package com.frankegan.foodsavers;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
@@ -49,8 +48,6 @@ public class MainActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
                 startActivity(new Intent(MainActivity.this, CreatePostActivity.class));
             }
         });

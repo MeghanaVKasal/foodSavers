70b46e3366ca1c99c7d3f7bd987bf807f7ba6c16
fegan104 <fegan104@gmail.com>
2018-12-05 21:50:52 -0500

    location tracking works

--- app/src/main/java/com/frankegan/foodsavers/MainActivity.java
+++ app/src/main/java/com/frankegan/foodsavers/MainActivity.java
@@ -1,14 +1,20 @@
 package com.frankegan.foodsavers;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Toast;
 
 import com.firebase.ui.auth.AuthUI;
 import com.firebase.ui.auth.ErrorCodes;
@@ -27,6 +33,7 @@ import java.util.HashMap;
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = MainActivity.class.getSimpleName();
     private static final int RC_SIGN_IN = 123;
+    private static final int PERMISSIONS_REQUEST = 100;
     private FirebaseAuth firebaseAuth;
     private FirebaseFirestore firestore;
     FirebaseInstanceId firebaseInstanceId;
@@ -51,6 +58,23 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(new Intent(MainActivity.this, CreatePostActivity.class));
             }
         });
+
+        //Check whether GPS tracking is enabled
+        LocationManager lm = (LocationManager) getSystemService(LOCATION_SERVICE);
+        if (!lm.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
+            finish();
+        }
+        //Check whether this app has access to the location permission//
+        int permission = ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION);
+        //If the location permission has been granted, then start the TrackerService//
+        if (permission == PackageManager.PERMISSION_GRANTED) {
+            startTrackerService();
+        } else {
+            //If the app doesn’t currently have access to the user’s location, then request access//
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                    PERMISSIONS_REQUEST);
+        }
     }
 
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
@@ -127,4 +151,25 @@ public class MainActivity extends AppCompatActivity {
                 return super.onOptionsItemSelected(item);
         }
     }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        //If the permission has been granted...//
+        if (requestCode == PERMISSIONS_REQUEST && grantResults.length == 1
+                && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            //...then start the GPS tracking service//
+            startTrackerService();
+        } else {
+            //If the user denies the permission request, then display a toast with some more information//
+            Toast.makeText(this, "Please enable location services to allow GPS tracking", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    //Start the TrackerService//
+    private void startTrackerService() {
+        startService(new Intent(this, TrackingService.class));
+        //Notify the user that tracking has been enabled//
+        Toast.makeText(this, "GPS tracking enabled", Toast.LENGTH_SHORT).show();
+    }
+
 }
---
+++ app/src/main/java/com/frankegan/foodsavers/TrackingService.java
@@ -0,0 +1,134 @@
+package com.frankegan.foodsavers;
+
+import android.Manifest;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.app.Service;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.os.Build;
+import android.os.IBinder;
+import android.support.v4.app.NotificationCompat;
+import android.support.v4.content.ContextCompat;
+import android.util.Log;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.GeoPoint;
+
+public class TrackingService extends Service {
+    public static final String LOCATION_CHANNEL = "LOCATION_CHANNEL";
+    private static final String TAG = TrackingService.class.getSimpleName();
+    private FirebaseAuth firebaseAuth;
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        firebaseAuth = FirebaseAuth.getInstance();
+        buildNotification();
+        loginToFirebase();
+    }
+
+    /**
+     * Create the persistent notification
+     */
+    private void buildNotification() {
+        // Create the NotificationChannel, but only on API 26+ because
+        // the NotificationChannel class is new and not in the support library
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            int importance = NotificationManager.IMPORTANCE_DEFAULT;
+            NotificationChannel channel = new NotificationChannel(LOCATION_CHANNEL, "Location Tracking", importance);
+            channel.setDescription("Alerts you when the app is tracking your location");
+            // Register the channel with the system; you can't change the importance
+            // or other notification behaviors after this
+            NotificationManager notificationManager = getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(channel);
+        }
+        String stop = "stop";
+        registerReceiver(stopReceiver, new IntentFilter(stop));
+        PendingIntent broadcastIntent = PendingIntent.getBroadcast(
+                this, 0, new Intent(stop), PendingIntent.FLAG_UPDATE_CURRENT);
+
+        // Create the persistent notification//
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, LOCATION_CHANNEL)
+                .setContentTitle(getString(R.string.app_name))
+                .setContentText(getString(R.string.tracking_enabled_notif))
+                .setOngoing(true)
+                .setContentIntent(broadcastIntent)
+                .setSmallIcon(R.drawable.ic_location_on_black_24dp);
+        startForeground(1, builder.build());
+    }
+
+    protected BroadcastReceiver stopReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            //Unregister the BroadcastReceiver when the notification is tapped
+            unregisterReceiver(stopReceiver);
+            //Stop the Service
+            stopSelf();
+        }
+    };
+
+    private void loginToFirebase() {
+        //Call OnCompleteListener if the user is signed in successfully//
+        FirebaseUser user = firebaseAuth.getCurrentUser();
+        //If the user has been authenticated...//
+        if (user != null) {
+            //...then call requestLocationUpdates//
+            requestLocationUpdates();
+        } else {
+            //If sign in fails, then log the error//
+            Log.d(TAG, "Firebase authentication failed");
+        }
+    }
+
+    /**
+     * Initiate the request to track the device's location//
+     */
+    private void requestLocationUpdates() {
+        LocationRequest request = new LocationRequest();
+        //Specify how often your app should request the device’s location
+        request.setInterval(10_000L);
+
+        //Get the most accurate location data available//
+        request.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        FusedLocationProviderClient client = LocationServices.getFusedLocationProviderClient(this);
+        int permission = ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION);
+
+        //If the app currently has access to the location permission...
+        if (permission == PackageManager.PERMISSION_GRANTED) {
+            //...then request location updates//
+            client.requestLocationUpdates(request, new LocationCallback() {
+                @Override
+                public void onLocationResult(LocationResult locationResult) {
+                    //Get a reference to the database, so your app can perform read and write operations//
+                    DocumentReference ref = FirebaseFirestore.getInstance()
+                            .collection("users")
+                            .document(firebaseAuth.getCurrentUser().getUid());
+                    Location location = locationResult.getLastLocation();
+                    if (location != null) {
+                        //Save the location data to the database
+                        ref.update("location", new GeoPoint(location.getLatitude(), location.getLongitude()));
+                    }
+                }
+            }, null);
+        }
+    }
+}

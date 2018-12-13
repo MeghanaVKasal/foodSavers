b1bd908e7430d622ede8b283b6f197d253def1a9
fegan104 <fegan104@gmail.com>
2018-12-13 16:45:09 -0500

    fixed cloud function

--- firebase-functions/functions/index.js
+++ firebase-functions/functions/index.js
@@ -10,17 +10,22 @@ const ONE_KM_DEG = 0.01270486596
 exports.notifyNewPost = functions.firestore
   .document('posts/{postId}')
   .onCreate((snap, context) => {
-    const { location } = snap.data()
+    console.log("snap data is :")
+    console.log(Object.keys(snap.data()))
+    const loc = snap.data().location
 
     console.log("new post location:")
-    console.log(location)
+    console.log(loc)
+    console.log("latitude is " + loc['_latitude'])
+    console.log("longitude is " + loc._longitude)
 
     return admin.firestore()
       .collection('users').get()
       .then(snap => snap.docs.filter(userSnap => {
-        const user = userSnap.data()
-        return Math.abs(location._latitude - user.location._latitude) < ONE_KM_DEG 
-              || Math.abs(location._longitude - user.location._longitude) < ONE_KM_DEG
+        const userLoc = userSnap.data().location
+        console.log("user is at " + userLoc)
+        return Math.abs(loc._latitude - userLoc._latitude) < ONE_KM_DEG 
+              || Math.abs(loc._longitude - userLoc._longitude) < ONE_KM_DEG
       }).map(u => {
         const token = u.get('fcmToken')
         console.log(`FCMToken= ${token}`)

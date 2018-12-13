cf65e4c9715ba9a84b4ffcd4b48c2935495890c7
meghana <meghana.v.kasal@gmail.com>
2018-12-12 22:16:24 -0500

    recycler view changes, rating added

--- app/src/main/java/com/frankegan/foodsavers/FoodDetailActivity.java
+++ app/src/main/java/com/frankegan/foodsavers/FoodDetailActivity.java
@@ -4,22 +4,33 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.RatingBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.HashMap;
+import java.util.Map;
+import android.support.annotation.NonNull;
 
 public class FoodDetailActivity extends AppCompatActivity {
     String mLatitude;
     String mLongitude;
 
     FirebaseFirestore firestore = FirebaseFirestore.getInstance();
-
+    private static final String TAG = "FoodDetailActivity";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -32,7 +43,8 @@ public class FoodDetailActivity extends AppCompatActivity {
         String foodTags = bundle.getString("bundleTags");
         String foodItemsCollection = bundle.getString("bundleFoodItems");
         String mDescription = bundle.getString("bundleDescription");
-        String mProducer = bundle.getString("bundleProducer");
+        final String mProducer = bundle.getString("bundleProducer");
+        final String mConsumer = bundle.getString("bundleConsumer");
         String mAddress = bundle.getString("bundleAddress");
         mLatitude = bundle.getString("bundleLatitude");
         mLongitude = bundle.getString("bundleLongitude");
@@ -80,5 +92,32 @@ public class FoodDetailActivity extends AppCompatActivity {
                 }
             }
         });
+
+        RatingBar mRatingBar = (RatingBar) findViewById(R.id.ratingBar);
+        mRatingBar.setOnRatingBarChangeListener(new RatingBar.OnRatingBarChangeListener() {
+            @Override
+            public void onRatingChanged(RatingBar ratingBar, float v, boolean b) {
+                Map<String, Object> rate = new HashMap<>();
+                rate.put("consumer", "users/" + FirebaseAuth.getInstance().getCurrentUser().getUid());
+                rate.put("producer", mProducer);
+                rate.put("stars", v);
+
+                firestore.collection("ratings")
+                        .add(rate)
+                        .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                            @Override
+                            public void onSuccess(DocumentReference documentReference) {
+                                Log.d(TAG, "DocumentSnapshot added with ID: " + documentReference.getId());
+                            }
+                        })
+                        .addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                Log.w(TAG, "Error adding document", e);
+                            }
+                        });
+            }
+        });
+
     }
 }
